b6aec944ba7779b9d06a7a2905fa5a75dde59805
Jonathan Sahagun <j-sahagun@live.com>
2024-01-14 14:20:35 -0800

    ik_test

---
+++ ik_test.py
@@ -0,0 +1,84 @@
+import tinyik
+
+print('start')
+
+arm = tinyik.Actuator([
+  'y', [0., 20., 0.], 
+  'z', [60., 0., 0.],
+  [0., 0., 11.],
+  # 'z', [-54., 0, 0.],
+  'z', [0.1,0,0], # 30 Degree
+  'y', [54,0,0],
+  [0., 0., 5.],
+  'z', [0., 1., 0.],
+  'y', [0., 11., 0.],
+])
+
+tinyik.visualize(arm, radius=2.5)
+
+
+print("ANG:", arm.angles)
+print("POS:", arm.ee)
+
+print('done')
+
+'''
+pip install tinyik -U
+pip install open3d
+
+
+'''
+
+# import numpy as np
+
+# import tinyik
+
+
+# tokens = [
+#     [.3, .0, .0], 'z', [.3, .0, .0], 'x', [.0, -.5, .0], 'x', [.0, -.5, .0]]
+
+
+# def visualize():
+#     leg = tinyik.Actuator(tokens)
+#     leg.angles = np.deg2rad([30, 45, -90])
+#     tinyik.visualize(leg)
+
+
+# def visualize_with_target():
+#     leg = tinyik.Actuator(tokens)
+#     leg.angles = np.deg2rad([30, 45, -90])
+#     tinyik.visualize(leg, target=[.8, .0, .8])
+
+
+# large_tokens = [
+#     [85., 80., 0.],
+#     'z',
+#     [500., 0., 0.],
+#     'z',
+#     [0., -500., 0.],
+# ]
+
+
+# def large_visualize():
+#     arm = tinyik.Actuator(large_tokens)
+#     tinyik.visualize(arm, radius=15.)
+
+
+# def large_visualize_with_target():
+#     arm = tinyik.Actuator(large_tokens)
+#     tinyik.visualize(arm, target=[400., -300., 0.], radius=15.)
+
+
+# def visualize_with_z_axis():
+#     arm = tinyik.Actuator([
+#         'z', [0, 0, 180.7], 'y', [-612.7, 0, 0], 'y', [-571.55, 0, 0],
+#         'y', [0, -174.15, 0], 'z', [0, 0, -119.85], 'y', [0, -116.55, 0]])
+#     tinyik.visualize(arm, radius=10.)
+
+
+# if __name__ == '__main__':
+#     visualize()
+#     visualize_with_target()
+#     large_visualize()
+#     large_visualize_with_target()
+#     visualize_with_z_axis()
